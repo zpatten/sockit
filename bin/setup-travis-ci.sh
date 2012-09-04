56c8e394033fd8d227c7f181b4a98cddff7fcfcd
Zachary Patten <zim931@gmail.com>
2012-09-03 20:24:54 -0700

    tweaking apt-get some more

--- bin/setup-travis-ci.sh
+++ bin/setup-travis-ci.sh
@@ -2,8 +2,8 @@
 set -x
 
 echo $PWD
-sudo apt-get -q -y --force-yes update
-sudo apt-get -q -y --force-yes install wget build-essential libpam0g-dev libbsd-dev
+sudo apt-get -qq -y --force-yes update
+sudo apt-get -q -y --force-yes install wget build-essential libpam0g-dev libbsd-dev libbsd0 libbsd0-dbg
 wget http://sourceforge.net/projects/ss5/files/ss5/3.8.9-6/ss5-3.8.9-6.tar.gz/download -O ss5-3.8.9.tar.gz
 tar -zxvf ss5-3.8.9.tar.gz
 cd ss5-3.8.9
