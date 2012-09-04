56899390b07a37639a85713b9542757b24a3ab65
Zachary Patten <zim931@gmail.com>
2012-09-03 20:08:09 -0700

    add build-essential to apt-get

--- bin/setup-travis-ci.sh
+++ bin/setup-travis-ci.sh
@@ -3,7 +3,7 @@ set -x
 
 echo $PWD
 sudo apt-get -qq -y --force-yes update
-sudo apt-get -qq -y --force-yes install wget
+sudo apt-get -qq -y --force-yes install wget build-essential
 sudo mkdir -p /usr/local/src
 cd /usr/local/src
 sudo wget http://sourceforge.net/projects/ss5/files/ss5/3.8.9-6/ss5-3.8.9-6.tar.gz/download -O ss5-3.8.9.tar.gz
