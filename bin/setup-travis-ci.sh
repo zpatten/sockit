a9b3cc887e1706d749787d897dad5be1658314d6
Zachary Patten <zim931@gmail.com>
2012-09-03 20:00:52 -0700

    cleanup travis-ci setup script

--- bin/setup-travis-ci.sh
+++ bin/setup-travis-ci.sh
@@ -2,19 +2,12 @@
 set -x
 
 echo $PWD
-ls -la
-sudo apt-get update -qq
-sudo apt-get install -qq wget
+sudo apt-get -qq -y --force-yes update
+sudo apt-get -qq -y --force-yes install wget
 sudo mkdir -p /usr/local/src
 cd /usr/local/src
-echo $PWD
-ls -la
 sudo wget http://sourceforge.net/projects/ss5/files/ss5/3.8.9-6/ss5-3.8.9-6.tar.gz/download -O ss5-3.8.9.tar.gz
-ls -la
 sudo tar -zxvf ss5-3.8.9.tar.gz
-ls -la
 cd ss5-3.8.9
-echo $PWD
-ls -la
 sudo ./configure
 sudo make install
