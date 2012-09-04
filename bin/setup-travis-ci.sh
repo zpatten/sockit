6f6ff096028ed6d448e0924fe1e89ae82d9b66b8
Zachary Patten <zim931@gmail.com>
2012-09-03 20:16:09 -0700

    tweaked travis-ci setup

--- bin/setup-travis-ci.sh
+++ bin/setup-travis-ci.sh
@@ -4,10 +4,8 @@ set -x
 echo $PWD
 sudo apt-get -y --force-yes update
 sudo apt-get -y --force-yes install wget build-essential libpam0g-dev
-sudo mkdir -p /usr/local/src
-cd /usr/local/src
-sudo wget http://sourceforge.net/projects/ss5/files/ss5/3.8.9-6/ss5-3.8.9-6.tar.gz/download -O ss5-3.8.9.tar.gz
-sudo tar -zxvf ss5-3.8.9.tar.gz
+wget http://sourceforge.net/projects/ss5/files/ss5/3.8.9-6/ss5-3.8.9-6.tar.gz/download -O ss5-3.8.9.tar.gz
+tar -zxvf ss5-3.8.9.tar.gz
 cd ss5-3.8.9
-sudo ./configure
-sudo make install
+./configure
+make
