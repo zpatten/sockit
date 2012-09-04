1fa6d305c3a413097bdb7f481634bd8cc44b5244
Zachary Patten <zim931@gmail.com>
2012-09-03 19:54:46 -0700

    trying to get travis-ci going

--- bin/setup-travis-ci.sh
+++ bin/setup-travis-ci.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env sh
 set -x
 
+echo $PWD
+ls -la
 sudo apt-get update -qq
 sudo apt-get install -qq wget
 sudo mkdir -p /usr/local/src
 cd /usr/local/src
-sudo wget http://sourceforge.net/projects/ss5/files/ss5/3.8.9-6/ss5-3.8.9-6.tar.gz/download
-sudo tar -zxvf ss5-3.8.9-6.tar.gz
+echo $PWD
+ls -la
+sudo wget http://sourceforge.net/projects/ss5/files/ss5/3.8.9-6/ss5-3.8.9-6.tar.gz/download -O ss5-3.8.9.tar.gz
+ls -la
+sudo tar -zxvf ss5-3.8.9.tar.gz
+ls -la
 cd ss5-3.8.9
+echo $PWD
+ls -la
 sudo ./configure
 sudo make install
