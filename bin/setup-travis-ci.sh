af4999749e06e6aa90147b1bf5af2e397015af4a
Zachary Patten <zachary@jovelabs.com>
2014-02-05 00:41:45 -0800

    travis: update script

--- bin/setup-travis-ci.sh
+++ bin/setup-travis-ci.sh
@@ -3,8 +3,8 @@ set -x
 
 echo $PWD
 apt-get -qy update
-apt-get -qy install wget build-essential libpam0g-dev libbsd-dev libbsd0 libbsd0-dbg
-wget http://sourceforge.net/projects/ss5/files/ss5/3.8.9-6/ss5-3.8.9-6.tar.gz/download -O ss5-3.8.9.tar.gz
+apt-get -qy install wget build-essential libpam0g-dev libbsd-dev libbsd0 libbsd0-dbg libssl-dev
+wget http://sourceforge.net/projects/ss5/files/ss5/3.8.9-8/ss5-3.8.9-8.tar.gz/download -O ss5-3.8.9.tar.gz
 tar -zxvf ss5-3.8.9.tar.gz
 cd ss5-3.8.9
 ./configure
