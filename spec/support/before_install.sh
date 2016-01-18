1106be61d88c8f743df8aeee2e133c12cf70f13e
Zachary Patten <zachary@jovelabs.com>
2016-01-17 16:25:08 -0800

    put the ss5 tarball into the repo so we will not have to worry about it suddenly disappearing in the future

--- spec/support/before_install.sh
+++ spec/support/before_install.sh
@@ -4,9 +4,8 @@ set -ex
 sudo apt-get -qy update
 sudo apt-get -qy install wget build-essential libpam0g-dev libbsd-dev libbsd0 libbsd0-dbg libssl-dev libldap2-dev
 
-wget http://sourceforge.net/projects/ss5/files/ss5/3.8.9-8/ss5-3.8.9-8.tar.gz/download -O ss5-3.8.9.tar.gz
-tar -zxvf ss5-3.8.9.tar.gz
-cd ss5-3.8.9
+tar -zxvf spec/support/ss5-3.8.9-8.tar.gz
+cd ss5*
 ./configure --with-configfile=/etc/ss5/ss5.conf
 sudo make
 sudo make install
