cf2df74c479049128fa7eb45424b98dc719393de
Zachary Patten <zpatten@jovelabs.io>
2016-04-08 01:10:39 -0700

    update email

--- lib/sockit/version.rb
+++ lib/sockit/version.rb
@@ -1,6 +1,6 @@
 ################################################################################
 #
-#      Author: Zachary Patten <zachary@jovelabs.net>
+#      Author: Zachary Patten <zpatten AT jovelabs DOT io>
 #   Copyright: Copyright (c) Zachary Patten
 #     License: Apache License, Version 2.0
 #
